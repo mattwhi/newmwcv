43d00e09658683266e2171b46db250364b7de348
mwhite <matthew.white@o2.com>
2015-06-25 16:15:39 +0100

    Adding Bootstrap

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -4,19 +4,27 @@ jQuery(document).ready(function($) {
 	//************************************************************************************//
 	//Dynamically add navigation buttons to side navigation dependant on number of sections
 	//************************************************************************************//
-	for(var i=0; i <= $section; i++);
 	$('section').each(function() {
 		$('.side-navigation').append('<li><a href="#"></a></li>');
 	});
 	//************************************************************************************//
+	//Dynamically add unique class to each section for nav to target
+	//************************************************************************************//
+	$('section').each(function(i) {
+		$(this).addClass('section' + (i));
+	});
+	//************************************************************************************//
+	//Dynamically add class'active' to first li in navigation
+	//************************************************************************************//
+	$('.side-navigation li:nth-child(1) a').addClass('active');
+	//************************************************************************************//
 	//Scrolls to section corresponding with the index of the link
 	//************************************************************************************//
-	$('.side-navigation li').click(function(){
-			var index = $('.side-navigation li').index(this);
-			console.log('this is index number' + " " + index);
-			$('html, body').animate({
-				scrollTop: $('section[i]').offset().top
-			})
+	$('.side-navigation li a').click(function(){
+			var index = $('.side-navigation li a').index(this);
+			$('.side-navigation li a').removeClass('active');
+			$(this).addClass('active');
+			$('html, body').animate({scrollTop: $('.section' + (index)).offset().top},800,'easeOutExpo')
 		});
 	//************************************************************************************//
 	//End
