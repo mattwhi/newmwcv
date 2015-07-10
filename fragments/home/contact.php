ad1ec9c2cf918262ecdc17c773213105b585242f
mwhite <matthew.white@o2.com>
2015-07-10 09:34:17 +0100

    MW

--- fragments/home/contact.php
+++ fragments/home/contact.php
@@ -1,21 +1,21 @@
 <section id="contact" class="contact" data-anchor="contact">
 	<div class="container">
 		<div class="row">
-		<div class="col-md-12 text-center">
+		<div class="col-md-12 text-center wow slideInLeft" style="visibility: hidden;">
 			<h2 class="section-header">Contact me</h2>
 			<div class="spacer"></div>
 			<p class="subtitle">You can contact me here</p>
 		</div>
-			<div class="col-md-6 col-xs-12">
+			<div class="col-md-6 col-xs-12 wow bounceInUp animated" style="visibility: hidden;">
 				<h2>Contact Information</h2>
 				<p><i class="fa fa-user"></i> Matthew White</p>
 				<p><i class="fa fa-mobile"></i> 07515 985590</p>
 				<p><i class="fa fa-envelope-o"></i> matthew@matthewianwhite.co.uk</p>
 			</div>
-			<div class="col-md-6 col-xs-12">
+			<div class="col-md-6 col-xs-12 wow slideInRight" style="visibility: hidden;">
 				<form>
 				<div class="form-group">
-				    <label for="name">Name</label>
+				    <label for="name">Name Hello World</label>
 				    <input type="text" class="form-control" id="nameInput" placeholder="Name">
 				  </div>
 				  <div class="form-group">
